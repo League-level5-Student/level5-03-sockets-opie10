7f5d7555c3e5cc9d650d227421fb4eb3fafad51a
opie10 <augie@keatinge.com>
2024-02-24 11:27:29 -0800

    serverchat
    chat kinda works its a bit weird but i think im happy with it

--- src/_02_Chat_Application/ChatApp.java
+++ src/_02_Chat_Application/ChatApp.java
@@ -4,6 +4,52 @@ package _02_Chat_Application;
  * Using the Click_Chat example, write an application that allows a server computer to chat with a client computer.
  */
 
-public class ChatApp {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+import _02_Chat_Application.ClientApp;
+import _02_Chat_Application.ServerApp;
+
+public class ChatApp extends JFrame {
+	JButton button = new JButton("CLICK");
+	
+	ServerApp server;
+	ClientApp client;
+	
+	
+	public static void main(String[] args) {
+		new ChatApp();
+	}
+	
+	public ChatApp(){
+		
+		int response = JOptionPane.showConfirmDialog(null, "Would you like to host a connection?", "Buttons!", JOptionPane.YES_NO_OPTION);
+		if(response == JOptionPane.YES_OPTION){
+			
+			server = new ServerApp(8080);
+			setTitle("SERVER");
+			JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+			String message = "";
+			message =JOptionPane.showInputDialog("Input message here:");
+			
+			server.sendMessage(message);
+			
+			server.start();
+			
+		}else{
+			setTitle("CLIENT");
+			String ipStr = JOptionPane.showInputDialog("Enter the IP Address");
+			String prtStr = JOptionPane.showInputDialog("Enter the port number");
+			int port = Integer.parseInt(prtStr);
+			client = new ClientApp(ipStr, port);
+			String message = "";
+			message =JOptionPane.showInputDialog("Input message here:");
+			
+			client.sendMessage(message);
+			
+			client.start();
+			
+		}
+	}
 }
